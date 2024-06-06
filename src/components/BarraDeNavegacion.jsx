55244309b23e351ad603b13ed8466b7a64e46111
Azner01 <azneraparicio@hotmail.com>
2024-06-05 22:06:23 -0500

    Azner (V.1.2)
    Boton de login y registro de usuario

--- src/components/BarraDeNavegacion.jsx
+++ src/components/BarraDeNavegacion.jsx
@@ -1,21 +1,25 @@
-import { URL } from '../constants/direccionesWeb'
+import { URL } from '../constants/DireccionesWeb'
+import ImagenLogin from '../components/ImagenLogin'
 import Boton from './Boton'
 import Enlace from './Enlace'
 
 export default function BarraDeNavegacion({ ocultarBotones, subtitulo }) {
 	return (
-		<nav className='flex flex-col items-center gap-5 bg-blue-700 p-7 text-white'>
+		<nav className='flex flex-col items-center gap-5 text-white bg-blue-700 p-7'>
 			<Enlace destino={URL.HOME} >
-				<h1 className='text-5xl text-center '>Sistema de Lockers UTP</h1>
+				<h1 className='text-5xl text-center'>Sistema de Lockers UTP</h1>
 			</Enlace>
 			{ocultarBotones ? (
 				<h2 className='text-5xl text-center '>{subtitulo}</h2>
 			) : (
-				<div className='grid w-full gap-5'>
+				<div className='flex justify-end w-full gap-5' >
 					<Enlace destino={URL.LOGIN}>
-						<Boton>Iniciar Sesion</Boton>
+						<Boton>
+							<h2>LOGIN</h2>
+							<ImagenLogin/>
+						</Boton>
 					</Enlace>
-					<Boton>Cerrar Sesion</Boton>
+					
 				</div>
 			)}
 		</nav>
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,7 +3,7 @@ import Boton from '../components/Boton'
 import Campo from '../components/Campo'
 import Enlace from '../components/Enlace'
 import Seccion from '../components/Seccion'
-import { URL } from '../constants/direccionesWeb'
+import { URL } from '../constants/DireccionesWeb'
 
 export default function Login() {
 	return (
@@ -12,7 +12,9 @@ export default function Login() {
 				ocultarBotones
 				subtitulo='LOGIN'
 			/>
-			<Seccion className='mt-4 text-2xl' gap="gap-4">
+			<nav className='flex items-start flex-raw'>
+			<Seccion className='py-4 mt-4 text-2xl' gap="gap-4">
+				<a className='mb-6 text-5xl text-slate-100'>Iniciar Sesión</a>
 				<Campo placeholder="Introducir Usuario"/>
 				<Campo placeholder="Introducir Contraseña" type="password"/>
 				<Enlace destino={URL.REGISTER}>
@@ -20,6 +22,28 @@ export default function Login() {
 				</Enlace>
 				<Boton className="w-full text-2xl">Iniciar sesión</Boton>
 			</Seccion>
+			<Seccion className='mt-4 text-2xl' gap="gap-4">
+				<a className='mb-6 text-5xl text-slate-100'>Registrarse</a>
+				<Campo placeholder="Nombre Completo"/>
+				<Campo placeholder="Nombre de Usuario"/>
+				<select className='w-full p-1 mt-1 border-2 form-input text-1xl rounded-xl'>
+					<option value="" disabled selected>Sexo...</option>
+					<option value="Hombre">Hombre</option>
+					<option value="Mujer">Mujer</option>
+				</select>
+				<select className='w-full p-1 mt-1 border-2 form-input text-1xl rounded-xl'>
+					<option value="" disabled selected>Tipo de Usuario...</option>
+					<option value="Hombre">Hombre</option>
+					<option value="Mujer">Mujer</option>
+				</select>
+				<Campo placeholder="Contraseña" type="password"/>
+				<Campo placeholder="Cédula"/>
+				<Campo placeholder="Correo"/>
+				<Campo placeholder="Teléfono"/>
+				<Boton className="w-full mb-6 text-2xl">Registrarse</Boton>
+			</Seccion>
+			</nav>
+			
 		</>
 	)
 }
