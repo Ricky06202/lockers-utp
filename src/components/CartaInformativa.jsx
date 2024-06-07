476d99bc37afaa8296b242b577ef62ce39ee0840
Azner01 <azneraparicio@hotmail.com>
2024-06-06 20:13:45 -0500

    Azner (2.2)
    Enlace de las carta informativa hacia la pagina de alquiler

--- src/components/CartaInformativa.jsx
+++ src/components/CartaInformativa.jsx
@@ -1,12 +1,17 @@
+import Enlace from "./Enlace";
+import { URL } from '../constants/DireccionesWeb'
+
 export default function CartaInformativa({ children, imagen, titulo}) {
 	return (
 		<div className='flex-row overflow-hidden bg-blue-600 border-4 border-blue-700 card'>
 			{imagen}
 			<div className='flex flex-col items-center justify-center bg-blue-500 sm:w-96'>
+			<Enlace destino={URL.RENT}>
 				<h3 className='text-2xl'>
 					{titulo}
 				</h3>
 				{children}
+				</Enlace>
 			</div>
 		</div>
 	)
