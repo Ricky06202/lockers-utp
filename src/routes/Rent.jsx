c525e7a1b6a52c194aa96a14f6bac4c743e06737
Azner01 <azneraparicio@hotmail.com>
2024-06-09 18:41:42 -0500

    1.6

--- src/routes/Rent.jsx
+++ src/routes/Rent.jsx
@@ -14,7 +14,7 @@ export default function Rent(){
             <nav className="m-8 bg-indigo-800">
             <Seccion>
             <form className='flex flex-col gap-5'>
-                <Campo placeholder="Locker" disabled/>
+                <Campo placeholder="Locker 1" disabled/>
                 <Campo placeholder="Duración (en días)" type="number"/>
                 <CampoSeleccion titulo="Método de Pago" opciones={["Yapi", "PayPal", "Visa"]}/>
                 <Boton className="mb-6" type="Submit">Alquilar</Boton>
